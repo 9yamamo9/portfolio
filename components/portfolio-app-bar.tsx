24d9bb95a5085fa4c20dea5ab9e7f191fba86991
yuta YAMAMORI <yamamori929yuta@gmail.com>
2022-01-07 16:00:02 +0900

    add: Welcomeページを作成した

--- components/portfolio-app-bar.tsx
+++ components/portfolio-app-bar.tsx
@@ -1,4 +1,4 @@
-import {AppBar, AppBarProps, Box, Button, IconButton, Toolbar, Typography} from '@mui/material';
+import {AppBar, AppBarProps, IconButton, Toolbar, Typography} from '@mui/material';
 import {Feed, GitHub, Person, Twitter} from '@mui/icons-material';
 
 export const PortfolioAppBar = (props: AppBarProps & { pageTitle: string }) => {
@@ -8,29 +8,27 @@ export const PortfolioAppBar = (props: AppBarProps & { pageTitle: string }) => {
       variant='outlined'
       sx={{backgroundColor: 'white'}}
     >
-      <Box sx={{flexGrow: 1}}>
-        <Toolbar>
-          <Typography
-            variant='h3'
-            color='black'
-            sx={{flexGrow: 1}}
-          >
-            {props.pageTitle}
-          </Typography>
-          <IconButton size='large'>
-            <Person/>
-          </IconButton>
-          <IconButton size='large' href='https://twitter.com/9yamamo9'>
-            <Twitter/>
-          </IconButton>
-          <IconButton size='large' href='https://github.com/9yamamo9'>
-            <GitHub />
-          </IconButton>
-          <IconButton size='large' href='https://ymmryt.hatenablog.com/'>
-            <Feed />
-          </IconButton>
-        </Toolbar>
-      </Box>
+      <Toolbar>
+        <Typography
+          variant='h3'
+          color='black'
+          sx={{flexGrow: 1}}
+        >
+          {props.pageTitle}
+        </Typography>
+        <IconButton size='large'>
+          <Person/>
+        </IconButton>
+        <IconButton size='large' href='https://twitter.com/9yamamo9'>
+          <Twitter/>
+        </IconButton>
+        <IconButton size='large' href='https://github.com/9yamamo9'>
+          <GitHub/>
+        </IconButton>
+        <IconButton size='large' href='https://ymmryt.hatenablog.com/'>
+          <Feed/>
+        </IconButton>
+      </Toolbar>
     </AppBar>
   )
 }
