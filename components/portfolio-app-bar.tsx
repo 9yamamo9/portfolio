05347f03603635aa076c29e552d9b5e00d6ef4d3
yuta YAMAMORI <yamamori929yuta@gmail.com>
2022-01-07 12:57:37 +0900

    add: AppBarに各種ページに遷移するMenuIconを追加した

--- components/portfolio-app-bar.tsx
+++ components/portfolio-app-bar.tsx
@@ -1,13 +1,36 @@
-import {AppBar, AppBarProps, Toolbar, Typography} from '@mui/material';
+import {AppBar, AppBarProps, Box, Button, IconButton, Toolbar, Typography} from '@mui/material';
+import {Feed, GitHub, Person, Twitter} from '@mui/icons-material';
 
-export const PortfolioAppBar = (props: AppBarProps & {pageTitle: string}) => {
+export const PortfolioAppBar = (props: AppBarProps & { pageTitle: string }) => {
   return (
-    <AppBar position='static' color='primary' variant='outlined'>
-      <Toolbar>
-        <Typography variant='h3'>
-          {props.pageTitle}
-        </Typography>
-      </Toolbar>
+    <AppBar
+      position='static'
+      variant='outlined'
+      sx={{backgroundColor: 'white'}}
+    >
+      <Box sx={{flexGrow: 1}}>
+        <Toolbar>
+          <Typography
+            variant='h3'
+            color='black'
+            sx={{flexGrow: 1}}
+          >
+            {props.pageTitle}
+          </Typography>
+          <IconButton size='large'>
+            <Person/>
+          </IconButton>
+          <IconButton size='large' href='https://twitter.com/9yamamo9'>
+            <Twitter/>
+          </IconButton>
+          <IconButton size='large' href='https://github.com/9yamamo9'>
+            <GitHub />
+          </IconButton>
+          <IconButton size='large' href='https://ymmryt.hatenablog.com/'>
+            <Feed />
+          </IconButton>
+        </Toolbar>
+      </Box>
     </AppBar>
   )
 }
