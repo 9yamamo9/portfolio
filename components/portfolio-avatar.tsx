b54b85e9b6a76d8a3ea9cadfae89613dc2b4e618
yuta YAMAMORI <yamamori929yuta@gmail.com>
2022-01-07 18:17:35 +0900

    add: プロフィール画像としてAvatarを追加した

---
+++ components/portfolio-avatar.tsx
@@ -0,0 +1,11 @@
+import {Avatar} from '@mui/material';
+
+export const PortfolioAvatar = () => {
+  return (
+    <Avatar
+      alt='Remy Sharp'
+      src='/yamamori_rice.jpg'
+      sx={{width: 160, height: 160}}
+    />
+  )
+}
